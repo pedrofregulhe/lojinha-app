c2d2ba24042329ede0093ec9f86d8ddcf4161fe9
pedrofregulhe <pedroofs@algartech.com>
2026-01-12 22:42:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ def tela_principal():
     
     col_info, col_acoes = st.columns([3, 1.1])
     with col_info:
-        st.markdown(f'<div class="header-style"><div style="display:flex; justify-content:space-between; align-items:center;"><div><h2 style="margin:0; color:white;">Olá, {u_nome}! 👋</h2><p style="margin:0; opacity:0.9; color:white;">Bem Vindo (a) a Loja Culligan.</p></div><div style="text-align:right; color:white;"><span style="font-size:12px; opacity:0.8;">SEU SALDO</span><br><span style="font-size:32px; font-weight:bold;">{sld:,.0f}</span> pts</div></div></div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="header-style"><div style="display:flex; justify-content:space-between; align-items:center;"><div><h2 style="margin:0; color:white;">Olá, {u_nome}! 👋</h2><p style="margin:0; opacity:0.9; color:white;">Bem Vindo (a) a Loja Culligan! Aqui você pode trocar seus pontos acumulados por prêmios incríveis, aproveite!</p></div><div style="text-align:right; color:white;"><span style="font-size:12px; opacity:0.8;">SEU SALDO</span><br><span style="font-size:32px; font-weight:bold;">{sld:,.0f}</span> pts</div></div></div>', unsafe_allow_html=True)
     
     with col_acoes:
         img_b64 = carregar_logo_base64(ARQUIVO_LOGO)
